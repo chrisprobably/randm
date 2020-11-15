da10b0b4397eed45e16725924b37a9ac0bc7eaac
christopherc <chris.clarke@caplin.com>
2020-11-15 00:23:26 +0000

    WIP: expanding dice functionality and adding ability to set the next result

--- randm.js
+++ randm.js
@@ -1,32 +1,67 @@
 const DIE = /(\d+)?d(\d+)([+|-]\d+)?/;
 
+const isMocked = (funcName) => randm.next[funcName].returnValues.length > 0;
+
+const mockedValue = (funcName) => randm.next[funcName].returnValues.pop();
+
 const randm = {
   any: () => Math.random(),
   bool: () => !!randm.int.between(0, 1),
-  coinFlip: () => randm.bool() ? 'heads' : 'tails',  
-  diceRoll: (die) => die ? randm.customDiceRoll(die) : randm.int.between(1, 6),
-  between: (x, y) => (randm.any() * (y - x)) + x,
-  oneIn: (n) => randm.int.between(1, n) === n,
-  from: (arry) => arry[randm.int.between(0, arry.length-1)],
-  happens: (times) => ({
-    outOf: (total) => {
-      return Array.from(Array(times).keys()).includes(randm.int.between(0, total - 1))
-    }
+  coinFlip: () => (randm.bool() ? "heads" : "tails"),
+  diceRoll: (die) =>
+    die ? randm.customDiceRoll(die) : randm.int.between(1, 6),
+  diceRollBeats: (target) => randm.diceRoll() >= target,
+  diceRollOf: (die) => ({
+    beats: (target) => randm.diceRollOf(die).isGreaterThanOrEqual(target),
+    isGreaterThan: (target) => randm.diceRoll(die) > target,
+    isGreaterThanOrEqual: (target) => randm.diceRoll(die) >= target,
+    isLessThan: (target) => randm.diceRoll(die) < target,
+    isLessThanOrEqual: (target) => randm.diceRoll(die) <= target,
+    rolls: () => {
+      if (!DIE.test(die))
+        throw new Error("Invalid die specifier, try one of these: d6, d3, 2d4");
+      const rolls = [];
+      const [, count = 1, size, modifier = 0] = DIE.exec(die);
+      for (let i = 0; i < count; i++) {
+        rolls.push(randm.int.between(1, size));
+      }
+      const unmodifiedTotal = rolls.reduce((sum, roll) => sum + roll);
+      return {
+        rolls,
+        unmodifiedTotal,
+        modifier,
+        total: unmodifiedTotal + parseInt(modifier),
+      };
+    },
   }),
-  percentageChance: (percentage) => randm.happens(Math.abs(percentage)).outOf(100),
-  customDiceRoll: (die) => {
-  	if (! DIE.test(die)) throw new Error('Invalid die specifier, try one of these: d6, d3, 2d4');
-  	const [ , count = 1, size, modifier = 0 ] = DIE.exec(die);
-  	let total = 0;
-  	for (let i = 0; i < count; i++) {
-  		total += randm.int.between(1, size)
-  	}
-  	return total + parseInt(modifier);
-  }
+  between: (x, y) => randm.any() * (y - x) + x,
+  oneIn: (n) => randm.int.between(1, n) === n,
+  from: (arry) => arry[randm.int.between(0, arry.length - 1)],
+  happens: (times) => ({
+    outOf: (total) =>
+      Array.from(Array(times).keys()).includes(randm.int.between(0, total - 1)),
+  }),
+  percentageChance: (percentage) =>
+    randm.happens(Math.abs(percentage)).outOf(100),
+  customDiceRoll: (die) => randm.diceRollOf(die).rolls().total,
 };
 
 randm.int = {
-  between: (x, y) => Math.round(randm.any() * (y - x)) + x
+  between: (x, y) => Math.round(randm.any() * (y - x)) + x,
 };
+
+randm.next = {};
+
+Object.keys(randm).forEach((prop) => {
+  if (typeof randm[prop] === "function") {
+    const originalFunc = randm[prop];
+    randm.next[prop] = {
+      returnValues: [],
+      returns: (val) => (randm.next[prop].returnValues = [val]),
+    };
+    randm[prop] = (...args) =>
+      isMocked(prop) ? mockedValue(prop) : originalFunc(...args);
+  }
+});
 
 module.exports = randm;
--- test/basic.js
+++ test/basic.js
@@ -1,186 +1,395 @@
-var randm = require('../randm')
-var t = require('tap')
+var randm = require("../randm");
+var t = require("tap");
 
-t.test('generates random number', function (t) {
+t.test("generates random number", function (t) {
   const rndNum = randm.any();
-  console.log('randm.any()', rndNum);
-  t.end()
-})
+  console.log("randm.any()", rndNum);
+  t.end();
+});
 
-t.test('generates random boolean', function (t) {
+t.test("generates random boolean", function (t) {
   const rndBoolean = randm.bool();
-  console.log('randm.bool()', rndBoolean);
-  t.type(rndBoolean, 'boolean');
-  t.end()
-})
+  console.log("randm.bool()", rndBoolean);
+  t.type(rndBoolean, "boolean");
+  t.end();
+});
 
-t.test('generates coin flip', function (t) {
+t.test("generates coin flip", function (t) {
   const coinFlip = randm.coinFlip();
-  console.log('randm.coinFlip()', coinFlip);
-  t.type(coinFlip, 'string');
+  console.log("randm.coinFlip()", coinFlip);
+  t.type(coinFlip, "string");
   t.matches(coinFlip, /heads|tails/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates a chance, which returns true on a one in N basis', function (t) {
+t.test("generates a chance, which returns true on a one in N basis", function (
+  t
+) {
   const chance = randm.oneIn(2);
-  t.type(chance, 'boolean');
-  console.log('randm.oneIn(2)', chance);
-  t.end()
-})
+  t.type(chance, "boolean");
+  console.log("randm.oneIn(2)", chance);
+  t.end();
+});
 
-t.test('randm.oneIn(1) always returns true', function (t) {
+t.test("randm.oneIn(1) always returns true", function (t) {
   const chance = randm.oneIn(1);
   t.equal(true, chance);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with a 6-sided die given no arguments', function (t) {
+t.test("generates dice roll with a 6-sided die given no arguments", function (
+  t
+) {
   const diceRoll = randm.diceRoll();
-  console.log('randm.diceRoll()', diceRoll);
+  console.log("randm.diceRoll()", diceRoll);
   t.matches(diceRoll, /[1-6]/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with the passed die size', function (t) {
-  const diceRoll = randm.diceRoll('d3');
+t.test("generates dice roll with the passed die size", function (t) {
+  const diceRoll = randm.diceRoll("d3");
   console.log('randm.diceRoll("d3")', diceRoll);
   t.matches(diceRoll, /[1-3]/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with the passed die size', function (t) {
-  const diceRoll = randm.diceRoll('d3');
+t.test("generates dice roll with the passed die size", function (t) {
+  const diceRoll = randm.diceRoll("d3");
   console.log('randm.diceRoll("d3")', diceRoll);
   t.matches(diceRoll, /[1-3]/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with the passed die count and size', function (t) {
-  const diceRoll = randm.diceRoll('2d1');
+t.test("generates dice roll with the passed die count and size", function (t) {
+  const diceRoll = randm.diceRoll("2d1");
   console.log('randm.diceRoll("2d1")', diceRoll);
   t.equal(2, diceRoll);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with the passed multi-digit die count and size', function (t) {
-  const diceRoll = randm.diceRoll('33d1');
-  console.log('randm.diceRoll("33d1")', diceRoll);
-  t.equal(33, diceRoll);
-  t.end()
-})
+t.test(
+  "generates dice roll with the passed multi-digit die count and size",
+  function (t) {
+    const diceRoll = randm.diceRoll("33d1");
+    console.log('randm.diceRoll("33d1")', diceRoll);
+    t.equal(33, diceRoll);
+    t.end();
+  }
+);
 
-t.test('generates dice roll with a positive modifier', function (t) {
-  const diceRoll = randm.diceRoll('d1+1');
+t.test("generates dice roll with a positive modifier", function (t) {
+  const diceRoll = randm.diceRoll("d1+1");
   console.log('randm.diceRoll("d1+1")', diceRoll);
   t.equal(2, diceRoll);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with a multi-digit positive modifier', function (t) {
-  const diceRoll = randm.diceRoll('d1+22');
+t.test("generates dice roll with a multi-digit positive modifier", function (
+  t
+) {
+  const diceRoll = randm.diceRoll("d1+22");
   console.log('randm.diceRoll("d1+22")', diceRoll);
   t.equal(23, diceRoll);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll for d3+2', function (t) {
-  const diceRoll = randm.diceRoll('d3+2');
+t.test("generates dice roll for d3+2", function (t) {
+  const diceRoll = randm.diceRoll("d3+2");
   console.log('randm.diceRoll("d3+2")', diceRoll);
   t.matches(diceRoll, /[3-5]/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with a negative modifier', function (t) {
-  const diceRoll = randm.diceRoll('d1-1');
+t.test("generates dice roll with a negative modifier", function (t) {
+  const diceRoll = randm.diceRoll("d1-1");
   console.log('randm.diceRoll("d1-1")', diceRoll);
   t.equal(0, diceRoll);
-  t.end()
-})
+  t.end();
+});
 
-t.test('generates dice roll with the passed 2d3', function (t) {
-  const diceRoll = randm.diceRoll('2d3');
+t.test("generates dice roll with the passed 2d3", function (t) {
+  const diceRoll = randm.diceRoll("2d3");
   console.log('randm.diceRoll("2d3")', diceRoll);
   t.matches(diceRoll, /[2-6]/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('throws an error with an incorrect die specifier', function (t) {
-  t.throws(() => randm.diceRoll('40'));
-  t.end()
-})
+t.test("throws an error with an incorrect die specifier", function (t) {
+  t.throws(() => randm.diceRoll("40"));
+  t.end();
+});
 
-t.test('generates random number between two values', function (t) {
+t.test("generates random number between two values", function (t) {
   t.equal(1, randm.between(1, 1));
   t.equal(-99, randm.between(-99, -99));
   t.equal(0, randm.between(0, 0));
 
-  console.log('randm.between(0, 10)', randm.between(0, 10));
-  t.end()
-})
+  console.log("randm.between(0, 10)", randm.between(0, 10));
+  t.end();
+});
 
-t.test('generates random int between two values', function (t) {
+t.test("generates random int between two values", function (t) {
   t.equal(1, randm.int.between(1, 1));
   t.equal(-99, randm.int.between(-99, -99));
   t.equal(0, randm.int.between(0, 0));
 
-  console.log('randm.int.between(0, 10)', randm.int.between(0, 10));
-  t.end()
-})
+  console.log("randm.int.between(0, 10)", randm.int.between(0, 10));
+  t.end();
+});
 
-t.test('random ints are integers', function (t) {
+t.test("random ints are integers", function (t) {
   t.ok(Number.isInteger(randm.int.between(1, 100)));
-  t.end()
-})
+  t.end();
+});
 
-t.test('random negative ints are integers', function (t) {
+t.test("random negative ints are integers", function (t) {
   t.ok(Number.isInteger(randm.int.between(-100, 0)));
-  t.end()
-})
+  t.end();
+});
 
-t.test('chooses a random value from an array', function (t) {
-  const val = randm.from(['foo', 'bar', 'qux']);
+t.test("chooses a random value from an array", function (t) {
+  const val = randm.from(["foo", "bar", "qux"]);
   console.log("randm.from(['foo', 'bar', 'qux'])", val);
-  t.type(val, 'string');
+  t.type(val, "string");
   t.matches(val, /foo|bar|qux/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('chooses a random number from an array', function (t) {
+t.test("chooses a random number from an array", function (t) {
   const val = randm.from([88, 626, 954]);
-  t.type(val, 'number');
+  t.type(val, "number");
   t.matches(val, /88|626|954/);
-  t.end()
-})
+  t.end();
+});
 
-t.test('always returns the same value when calling from on a single element array', function (t) {
-  const val = randm.from(['narnia']);
-  t.equal(val, 'narnia');
-  t.end()
-})
+t.test(
+  "always returns the same value when calling from on a single element array",
+  function (t) {
+    const val = randm.from(["narnia"]);
+    t.equal(val, "narnia");
+    t.end();
+  }
+);
 
-t.test('happens one out of one is always true', function (t) {
+t.test("happens one out of one is always true", function (t) {
   const val = randm.happens(1).outOf(1);
   t.equal(val, true);
-  t.end()
-})
+  t.end();
+});
 
-t.test('happens zero out of ten is always false', function (t) {
+t.test("happens zero out of ten is always false", function (t) {
   const val = randm.happens(0).outOf(10);
   t.equal(val, false);
-  t.end()
-})
+  t.end();
+});
 
-t.test('perecentageChance of 100% always returns true', function (t) {
+t.test("perecentageChance of 100% always returns true", function (t) {
   const val = randm.percentageChance(100);
   t.equal(val, true);
-  t.end()
-})
+  t.end();
+});
 
-t.test('perecentageChance of 0% always returns false', function (t) {
+t.test("perecentageChance of 0% always returns false", function (t) {
   const val = randm.percentageChance(0);
   t.equal(val, false);
-  t.end()
-})
+  t.end();
+});
 
+t.test("next sets the next diceRoll result", function (t) {
+  randm.next.diceRoll.returns(3);
+  const val = randm.diceRoll();
+  t.equal(val, 3);
+  t.end();
+});
 
+t.test(
+  "next sets the next diceRoll result but does not affect the subsequent one",
+  function (t) {
+    randm.next.diceRoll.returns(99);
+    t.equal(randm.diceRoll(), 99);
+    const subsequentRoll = randm.diceRoll();
+    t.notEqual(randm.diceRoll(), 99);
+    t.ok(subsequentRoll >= 1 && subsequentRoll <= 6);
+    t.end();
+  }
+);
+
+t.test("calling returns overwrites the previous return value", function (t) {
+  randm.next.diceRoll.returns(99);
+  randm.next.diceRoll.returns(101);
+  t.equal(randm.diceRoll(), 101);
+  const subsequentRoll = randm.diceRoll();
+  t.ok(subsequentRoll >= 1 && subsequentRoll <= 6);
+  t.end();
+});
+
+t.test("next sets the next any result", function (t) {
+  randm.next.any.returns(0.12345);
+  const val = randm.any();
+  t.equal(val, 0.12345);
+  t.end();
+});
+
+t.test("next sets the next bool result as false", function (t) {
+  randm.next.bool.returns(false);
+  const val = randm.bool();
+  t.equal(val, false);
+  t.end();
+});
+
+t.test("next sets the next bool result as true", function (t) {
+  randm.next.bool.returns(true);
+  const val = randm.bool();
+  t.equal(val, true);
+  t.end();
+});
+
+t.test("next sets the next coinFlip result as tails", function (t) {
+  randm.next.coinFlip.returns("tails");
+  const val = randm.coinFlip();
+  t.equal(val, "tails");
+  t.end();
+});
+
+t.test("next sets the next coinFlip result as heads", function (t) {
+  randm.next.coinFlip.returns("heads");
+  const val = randm.coinFlip();
+  t.equal(val, "heads");
+  t.end();
+});
+
+t.test("next sets the next between result", function (t) {
+  randm.next.between.returns(5);
+  const val = randm.between(1, 4);
+  t.equal(val, 5);
+  t.end();
+});
+
+t.test(
+  "diceRollBeats returns true when a diceRoll is equal or greater than the passed value",
+  function (t) {
+    t.ok(randm.diceRollBeats(1));
+    console.log("randm.diceRollBeats(4)", randm.diceRollBeats(4));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollBeats returns false when a diceRoll is not equal or greater than the passed value",
+  function (t) {
+    t.notOk(randm.diceRollBeats(7));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isGreaterThanOrEqual() returns true when a diceRoll of the passed die count is equal or greater than the target",
+  function (t) {
+    t.ok(randm.diceRollOf("d3+6").isGreaterThanOrEqual(7));
+    console.log(
+      "randm.diceRollOf('3d6').isGreaterThanOrEqual(4)",
+      randm.diceRollOf("3d6").isGreaterThanOrEqual(4)
+    );
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isGreaterThanOrEqual() returns false when a diceRoll of the passed die count is not equal or greater than the target",
+  function (t) {
+    t.notOk(randm.diceRollOf("d1").isGreaterThanOrEqual(2));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().beats() returns true when a diceRoll of the passed die count is equal or greater than the target",
+  function (t) {
+    t.ok(randm.diceRollOf("d3+6").beats(7));
+    console.log(
+      "randm.diceRollOf('3d6').beats(4)",
+      randm.diceRollOf("3d6").beats(4)
+    );
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().beats() returns false when a diceRoll of the passed die count is not equal or greater than the target",
+  function (t) {
+    t.notOk(randm.diceRollOf("d1").beats(2));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isGreaterThan() returns true when a diceRoll of the passed die count is greater than the target",
+  function (t) {
+    t.ok(randm.diceRollOf("d3+6").isGreaterThan(6));
+    console.log(
+      "randm.diceRollOf('3d6').isGreaterThan(4)",
+      randm.diceRollOf("3d6").isGreaterThan(4)
+    );
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isGreaterThan() returns false when a diceRoll of the passed die count is not greater than the target",
+  function (t) {
+    t.notOk(randm.diceRollOf("d1").isGreaterThan(1));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isLessThan() returns true when a diceRoll of the passed die count is less than the target",
+  function (t) {
+    t.ok(randm.diceRollOf("d6").isLessThan(7));
+    console.log(
+      "randm.diceRollOf('d6').isLessThan(4)",
+      randm.diceRollOf("d6").isLessThan(4)
+    );
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isLessThan() returns false when a diceRoll of the passed die count is not less than the target",
+  function (t) {
+    t.notOk(randm.diceRollOf("3d6+2").isLessThan(5));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isLessThanOrEqual() returns true when a diceRoll of the passed die count is less than or equal to the target",
+  function (t) {
+    t.ok(randm.diceRollOf("d1").isLessThanOrEqual(1));
+    console.log(
+      "randm.diceRollOf('d6').isLessThanOrEqual(4)",
+      randm.diceRollOf("d6").isLessThanOrEqual(4)
+    );
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().isLessThanOrEqual() returns false when a diceRoll of the passed die count is not less than or equal to the target",
+  function (t) {
+    t.notOk(randm.diceRollOf("3d6+2").isLessThanOrEqual(6));
+    t.end();
+  }
+);
+
+t.test(
+  "diceRollOf().rolls() returns an object containing the total and the individual dice rolls",
+  function (t) {
+    const { total, rolls } = randm.diceRollOf("3d6+2").rolls();
+    console.log(
+      'randm.diceRollOf("3d6+2").rolls()',
+      randm.diceRollOf("3d6+2").rolls()
+    );
+    t.end();
+  }
+);
